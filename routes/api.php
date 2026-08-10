c0b20d145dd90b75edee700e28fb1ff6b0be4f3d
Ananya <ananyarkoshe@gmail.com>
2026-08-10 10:57:33 +0530

    Save my current changes

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\HotelController;
 
 Route::get('/hotels', [HotelController::class, 'index']);
+Route::get('/hotels/{id}', [HotelController::class, 'show']);
